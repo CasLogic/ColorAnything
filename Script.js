d844230ee7bd30f35040c41370bfa224e26f2f62
CasLogic <c.casius@yahoo.com>
2015-07-29 20:06:06 -0400

    Site for Class Project

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Color NE-Thing. <b>You Can Color Anything</b></p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" + 
+"<li><a href='Index.htm'>Home</a></li>" +
+"<li><a href='About.htm'>Photos</a></li>" + 
+"<li><a href='Customer.htm'>Members</a></li>" + "</ul>";
